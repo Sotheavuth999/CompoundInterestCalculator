a6363d264b00d1d2e652bc38d30689fc34de070e
Sotheavuth999 <sotheavuthn@yahoo.com>
2016-10-07 22:56:57 +0300

    first commit

--- Compound Interest Calculator/ViewController.swift
+++ Compound Interest Calculator/ViewController.swift
@@ -9,6 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var lblTitleOUTLET: UILabel!
+    @IBOutlet weak var sgmMissingVariableOUTLET: UISegmentedControl!
+    @IBOutlet weak var txtField1OUTLET: UITextField!
+    @IBOutlet weak var txtField2OUTLET: UITextField!
+    @IBOutlet weak var txtField3OUTLET: UITextField!
+    @IBOutlet weak var txtField4OUTLET: UITextField!
+    @IBOutlet weak var btnCalculateOUTLET: UIButton!
+    @IBOutlet weak var lblFinalAnswerOUTLET: UILabel!
+    
+    
+    var missingVariable : String?
+    var variable1 : Double?
+    var varialbe2 : Double?
+    var variable3 : Double?
+    var variable4 : Double?
+    var finalAnswer : Double?
+    var units : String?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +37,78 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func chooseMissingVariableSegmentACTION(_ sender: AnyObject) {
+        txtField2OUTLET.placeholder = "Percent interest rate"
+        txtField3OUTLET.placeholder = "Times compounded per year"
+        switch sgmMissingVariableOUTLET.selectedSegmentIndex {
+        case 0:
+            missingVariable = "Final amount"
+            txtField1OUTLET.placeholder = "Principle amount"
+            txtField4OUTLET.placeholder = "Years invested"
+        case 1:
+            missingVariable = "Principal amount"
+            txtField1OUTLET.placeholder = "Principle amount"
+            txtField4OUTLET.placeholder = "Years invested"
+        case 2:
+            missingVariable = "Years invested"
+            txtField1OUTLET.placeholder = "Principal amount"
+            txtField4OUTLET.placeholder = "Final amount"
+        default:
+            break
+        }
+    }
+    @IBAction func pressCalculateButtonACTION(_ sender: AnyObject) {
+        if assignValues() == true {
+            switch missingVariable! {
+            case "Final amount":
+                calculateFinalAmount()
+            case "Principal amount":
+                calculatePrincipalAmount()
+            case "Years invested":
+                calculateYearsInvested()
+            default:
+                break
+            }
+            
+            displayFinalAnswer(variable: missingVariable!)
 
+        }
+    }
+    
+    func assignValues() -> Bool {
+        variable1 = Double(txtField1OUTLET.text!)
+        varialbe2 = Double(txtField2OUTLET.text!)! / 100
+        variable3 = Double(txtField3OUTLET.text!)
+        variable4 = Double(txtField4OUTLET.text!)
+        return true
+    }
+    
+    func calculateFinalAmount() {
+        // A = P (1 + r / n)^nt
+        // variable1 = p, variable2 = r, variable3 = n, variable4 = t
+        finalAnswer = variable1! * pow(1+(varialbe2!/variable3!),(variable3!*variable4!))
+        units = " dollars."
+    }
+    
+    func calculatePrincipalAmount() {
+        // P = A / (1 + r / n)^nt
+        // variable1 = A, variable2 = r, variable3 = n, variable4 = t
+        finalAnswer = variable1! / pow(1+(varialbe2!/variable3!),(variable3!*variable4!))
+        units = " dollars."
+    }
+    
+    func calculateYearsInvested() {
+        // (log(A/P) / log(1+r/n)) / n
+        // variable1 = p, variable2 = r, variable3 = n, variable4 = A
+        finalAnswer = (log(variable4!/variable1!)) / (log(1+(variable1!/variable3!)))
+        finalAnswer = finalAnswer! /  variable3!
+        units =  " years."
+    }
+    
+    func displayFinalAnswer(variable : String) {
+        lblFinalAnswerOUTLET.text = variable + ": " + String(format: "%0.2f", finalAnswer!) + units!
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,14 @@
+# Compound Interest Calculator
+
+Compound interest is interest calculated on the initial principal and also on the accumulated interest of previous periods of a deposit or loan.
+
+Annual Compound Interest Formula:
+A = P(1+r/n)^n
+
+Where:
+
+A = the future value of the investment/loan, including interest
+P = the principal investment amount (the initial deposit or loan amount)
+r = the annual interest rate (decimal)
+n = the number of times that interest is compounded per year
+t = the number of years the money is invested or borrowed for
